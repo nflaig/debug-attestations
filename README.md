eef7cc28bdc17014f3be50ef1ae6828642e55f87
Nico Flaig <nflaig@protonmail.com>
2024-07-12 13:32:54 +0100

    Add script to get validator indexes for list of pubkeys

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ Get all attestations of epoch
 ```sh
 node attestations-of-epoch.js validator_indexes_example.txt 209705
 ```
+
+Get validator indexes for list of pubkeys
+
+```sh
+node pubkeys-to-indexes.js ./data/validator_pubkeys.txt ./data/validator_indexes.txt
+```
---
+++ pubkeys-to-indexes.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const axios = require("axios");
+
+async function fetchValidators(pubkeys) {
+  const BASE_URL = "https://lodestar-mainnet.chainsafe.io";
+  const state_id = "head";
+  const pubkeysNoPrefix = pubkeys.map((p) => p.replace("0x", ""));
+  const url = `${BASE_URL}/eth/v1/beacon/states/${state_id}/validators?id=0x${pubkeysNoPrefix.join(",0x")}`;
+  const response = await axios.get(url);
+
+  if (response.data && response.data.data) {
+    return response.data.data;
+  } else {
+    throw new Error("Invalid API response");
+  }
+}
+
+async function main() {
+  const args = process.argv.slice(2);
+  const inputFile = args[0];
+  const pubkeys = fs.readFileSync(inputFile, "utf-8").trim().split("\n");
+
+  const validatorsData = await fetchValidators(pubkeys);
+
+  let indexesList = "";
+  for (const data of validatorsData) {
+    indexesList += `${data.index}\n`;
+  }
+
+  const outputFile = args[1] ?? `./data/validator_indexes.txt`;
+
+  fs.writeFileSync(outputFile, indexesList);
+  console.log(`Validator indexes saved to ${outputFile}`);
+}
+
+main().catch((error) => {
+  console.error("An error occurred:", error.message);
+});
